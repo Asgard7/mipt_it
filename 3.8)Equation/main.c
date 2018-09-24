31e004f37aded6de9c81349435e3e0e8b18c5fe0
Asgard7 <sams.777@yandex.ru>
2018-09-24 11:30:00 +0300

    finished ver.1

--- 3.8)Equation/main.c
+++ 3.8)Equation/main.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 
+const double pi = 3.145926;
+
 double f(double x);
+double mod(double a);
+double make_interval(double *a, double *b);
 
 int main(void)
 {
+
     double a, b;
     printf("Enter an interval(separated by a space): ");
     scanf("%lf %lf", &a, &b);
-    double x, c, eps = 1.0e-11;
-    if(f(a) * f(b) <= 0)
+    double *x = &a, *y = &b;
+    make_interval(x, y);
+    double c, eps = 1.0e-11;
+    if(f(*x) * f(*y) <= 0)
     {
-        while((b - a) >= eps)
+        while(mod(*y - *x) >= eps)
         {
-            c = (a + b) / 2;
-            if(f(a) * f(c) <= 0)
-                b = c;
+            c = (*x + *y) / 2;
+            if(f(*x) * f(c) <= 0)
+                *y = c;
             else
-                a = c;
+                *x = c;
         }
         printf("root x = %.10f", c);
     }
@@ -29,4 +36,30 @@ int main(void)
 double f(double x)
 {
     return x*cos(x) - sin(x);
+}
+
+double mod(double a)
+{
+    if(a < 0)
+        return (-a);
+    else
+        return a;
+}
+
+double make_interval(double *a, double *b)
+{
+    if(mod(*a) > mod(*b))
+    {
+        if(*a > 0)
+            *b = *a - (pi/2 + pi);
+        else
+            *b = *a + (pi/2 + pi);
+    }
+    else
+    {
+        if(*b > 0)
+            *a = *b - (pi/2 + pi);
+        else
+            *a = *b + (pi/2 + pi);
+    }
 }
