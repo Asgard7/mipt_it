b68f04c84fd1080abdaacd8e30520edd2ec90437
Asgard7 <43243989+asgard7@users.noreply.github.com>
2018-10-08 16:13:39 +0300

    final ver.2

--- 3.6)area_ver2/main.c
+++ 3.6)area_ver2/main.c
@@ -1,39 +1,43 @@
+//решение основано на формуле площади Гаусса
 #include <stdio.h>
 #include <math.h>
 
 typedef struct point {
-    int x;
-    int y;
+    double x;
+    double y;
 }Point;
 
-int mod(int a);
+double mod(double a);
 
-void get_struct(Point *rectangle);
+void get_struct(Point *rectangle); //функция для получения координат точки
+void get_area(Point rectangle[4]);
 
 int main()
 {
     Point rectangle[4];
     get_struct(rectangle);
-    int a = mod(rectangle[0].x - rectangle[1].x);
-    int b = mod(rectangle[0].y - rectangle[1].y);
-    int a1 = mod(rectangle[1].x - rectangle[2].x);
-    int b1 = mod(rectangle[1].y - rectangle[2].y);
-    printf("area = %f", sqrt((a*a + b*b) * (a1*a1 + b1*b1)));
+    get_area(rectangle);
     return 0;
 }
 
-int mod(int a) //функция для расчета модуля числа
+double mod(double a) //функция для расчета модуля числа
 {
     if(a < 0)
         a = -a;
     return a;
 }
 
-void get_struct(Point *rectangle) //функция для получения координат точки
+void get_struct(Point *rectangle)
 {
     printf("Enter coordinates(in pairs in space, e.g 1 1 2 2 3 3 4 4): ");
     for (int i = 0; i < 4; i++)
     {
-        scanf("%d %d", &rectangle[i].x, &rectangle[i].y);
+        scanf("%lf %lf", &rectangle[i].x, &rectangle[i].y);
     }
+}
+
+void get_area(Point rectangle[4])
+{
+    double area = mod(rectangle[0].x * rectangle[1].y + rectangle[1].x * rectangle[2].y + rectangle[2].x * rectangle[0].y - rectangle[1].x * rectangle[0].y - rectangle[2].x * rectangle[1].y - rectangle[0].x * rectangle[2].y);
+    printf("area = %f", area);
 }
