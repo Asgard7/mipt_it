5cdd80ca71bdc8b21e20992b7864836f324788c1
Asgard7 <43243989+asgard7@users.noreply.github.com>
2018-10-08 17:49:48 +0300

    in progress

---
+++ 4.2)section/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct section{
+    int x1;
+    int x2;
+}Section;
+
+void get_struct(Section *A, int k);
+
+int main()
+{
+    int n, i;
+    printf("Enter number of section: ");
+    scanf("%d", &n);
+    Section A[n];
+    get_struct(A, n);
+    printf("Hello, World!\n");
+    return 0;
+}
+
+void get_struct(Section *A, int k)
+{
+    printf("Enter x1 and x2 by space: ");
+    for (int i = 0; i < k; i++)
+        scanf("%d %d", &A[i].x1, &A[i].x2);
+}
