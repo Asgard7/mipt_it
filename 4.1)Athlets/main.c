044a336c92976028ef997b619eacdba71179b396
Asgard7 <sams.777@yandex.ru>
2018-10-08 14:33:50 +0300

    final ver.1

--- 4.1)Athlets/main.c
+++ 4.1)Athlets/main.c
@@ -4,7 +4,7 @@
 typedef struct athlet { //структура представляющая атлета с массой m и силой n
     int m;
     int n;
-} Athlet;
+}Athlet;
 
 int cmp_n(const void *a, const void *b); //компаратор силы
 int m_sum(Athlet *A, int n); //функция считающая общую массу заданного диапазона атлетов
@@ -13,20 +13,27 @@ void get_struct(Athlet *A, int k); //функция для получения м
 void sort_struct_n(Athlet *A, int k); //функция для сортировки атлетов по силе
 void print_struct(Athlet *A, int n); //ункция для печати заданного диапазона атлетов
 
-int main() {
+int main()
+{
     int k, i = 0, s;
     scanf("%d", &k);
     Athlet A[k]; //создаем массив структур типа атлет
     get_struct(A, k);
     sort_struct_n(A, k);
-    while (i < k) {
+    while(i < k)
+    {
         s = m_sum(A, i);
-        if (i == k - 1) {
+        if(i == k - 1)
+        {
             print_struct(A, i);
             return 0;
-        } else if (s <= A[i + 1].n) {
+        }
+        else if(s <= A[i + 1].n)
+        {
             i++;
-        } else {
+        }
+        else
+        {
             print_struct(A, i);
             return 0;
         }
@@ -34,29 +41,35 @@ int main() {
     return 0;
 }
 
-int cmp_n(const void *a, const void *b) {
-    return (((Athlet *) a)->n - ((Athlet *) b)->n);
+int cmp_n(const void *a, const void *b)
+{
+    return (((Athlet*)a)->n - ((Athlet*)b)->n);
 }
 
-int m_sum(Athlet *A, int n) {
+int m_sum(Athlet *A, int n)
+{
     int s = 0;
-    for (int m = 0; m <= n; m++) {
+    for (int m = 0; m <= n; m++)
+    {
         s = s + A[m].m;
     }
     return s;
 }
 
-void get_struct(Athlet *A, int k) {
+void get_struct(Athlet *A, int k)
+{
     printf("Enter mass and force by space: ");
     for (int i = 0; i < k; i++)
         scanf("%d %d", &A[i].m, &A[i].n);
 }
 
-void sort_struct_n(Athlet *A, int k) {
+void sort_struct_n(Athlet *A, int k)
+{
     qsort(A, k, sizeof(A[0]), cmp_n);
 }
 
-void print_struct(Athlet *A, int n) {
+void print_struct(Athlet *A, int n)
+{
     for (int i = 0; i <= n; i++)
         printf("%d - %d:%d\n", i, A[i].m, A[i].n); //печатает атлетов в столбик в виде №атлета - масса:сила
 }
